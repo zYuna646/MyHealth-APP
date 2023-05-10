98f369981f8e8f07fd62ca7f17b068b686e4639d
zYuna646 <rangga_s1sisfo@mahsiswa.ung.ac.id>
2023-05-11 04:42:31 +0800

    inisialisasi project dan menambahkan screen (home, login, register)

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
+import 'react-native-gesture-handler';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+
+
+
 
 export default function App() {
   return (
