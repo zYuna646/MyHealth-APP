d684cebf0d7eeb5d39beee26a53fb8c95a683a34
zYuna646 <rangga_s1sisfo@mahsiswa.ung.ac.id>
2023-05-12 21:15:30 +0800

    updated

--- screen/Tabs.js
+++ screen/Tabs.js
@@ -3,18 +3,69 @@ import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import Home from './Home'
 import MainHome from './MainHome'
+import Profile from './Profile'
+import { Foundation, MaterialIcons, Entypo, Fontisto } from '@expo/vector-icons';
 
+import { LinearGradient } from 'expo-linear-gradient'
 export default function Tabs() {
   const Tab = createBottomTabNavigator()
   return (
 
-    <Tab.Navigator>
-      <Tab.Screen 
-        name='MainScreen'
+    <Tab.Navigator
+      screenOptions={{
+        tabBarBackground: () => (
+          <LinearGradient
+            colors={['#A5E5E3', '#62CFCB']}
+            style={{ flex: 1 }}
+            start={{ x: 0, y: 0.5 }}
+            end={{ x: 1, y: 0.5 }}
+          />
+        ),
+        tabBarShowLabel: false,
+        headerShown: false,
+        tabBarActiveTintColor: 'black',
+        tabBarInactiveTintColor: 'white'
+      }}
+
+    >
+      <Tab.Screen
+        name='Home'
         component={MainHome}
+        options={{
+          tabBarIcon: (props) => (
+            <Foundation name="home" size={30} color={props.color} />
+          )
+        }}
+      />
+      <Tab.Screen
+        name='Form'
+        component={MainHome}
+        options={{
+          tabBarIcon: (props) => (
+            <Fontisto name="mic" size={30} color={props.color} />
+          )
+        }}
+      />
+      <Tab.Screen
+        name='Profile'
+        component={Profile}
+        options={{
+          tabBarIcon: (props) => (
+            <MaterialIcons name="account-circle" size={30} color={props.color} />
+          )
+        }}
+      />
+      <Tab.Screen
+        name='Keluar'
+        component={MainHome}
+        options={{
+          tabBarIcon: (props) => (
+            <Entypo name="log-out" size={30} color={props.color} />
+          )
+        }}
       />
     </Tab.Navigator>
-  
+
   )
 }
 
